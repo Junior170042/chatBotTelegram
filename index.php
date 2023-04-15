0192809a956eb287a672d90f9e534a92933e3331
J7coder <73503465+Junior170042@users.noreply.github.com>
2023-04-15 13:55:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ include './bot..php';
 
 $input = file_get_contents('php://input');
 $update = json_decode($input, TRUE);
-/*$chat_id = $update['message']['chat']['id'] || '6092238612';
+/*$chat_id = $update['message']['chat']['id'];
 $message = $update['message']['text'];*/
 
-$automateBot = new AutomateBot('hay jamón?', '6092238612');
+$automateBot = new AutomateBot('hay jamón?', chatId);
 
 $automateBot->replyMessage();
